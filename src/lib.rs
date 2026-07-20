7beb9e9b75f47002a3e99d13ddc64987655035bb
deputty <258258427+deputty@users.noreply.github.com>
2026-07-20 14:17:15 +0000

    chore: remove logs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use base62::decode;
 use http::StatusCode;
 use worker::event;
-use worker::console_log;
 use worker::{Env, Context, Result};
 use worker::{Request, Response};
 
@@ -13,9 +12,7 @@ async fn fetch(req: Request, env: Env, _ctx: Context) -> Result<Response> {
         .and_then(|mut seg| seg.next())
         .unwrap_or_default();
     let kv = env.kv("KV")?;
-    console_log!("slug {}", slug);
     if let Ok(id) = decode(slug) {
-        console_log!("id {}", id);
         if let Some(text) = kv.get(&format!("pastes:{}", id)).text().await? {
             Response::ok(text)
         } else {
